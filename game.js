888415847a094fd9a531d2ed346ac7389b265650
Caz vonKow <skopsycats@gmail.com>
2011-03-09 12:46:17 -0500

    Game working to the max, level advancement and everything. Just need to add undo/restart, move counter, etc.

--- game.js
+++ game.js
@@ -3,14 +3,39 @@
 
 	var lvls = [
 		[
-			[6,1,1,1,1,6],
-			[1,1,0,0,1,6],
-			[1,4,2,0,1,6],
-			[1,1,2,0,1,1],
-			[1,1,0,2,0,1],
-			[1,3,2,0,0,1],
-			[1,3,3,5,3,1],
-			[1,1,1,1,1,1]
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6]
+		], [
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,1,1,1,1,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,1,1,0,0,1,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,1,4,2,0,1,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,1,1,2,0,1,1,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,1,1,0,2,0,1,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,1,3,2,0,0,1,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,1,3,3,5,3,1,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,1,1,1,1,1,1,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
+			[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6]
 		]
 	];
 	function game(lData) {
@@ -31,19 +56,13 @@
 		}
 		function checkMove(d) {
 			if (d=='u') {
-				// square and square above
-				var c = that.lvl[tY][tX],
-					t = that.lvl[tY-1][tX];
-				// if targ empty or goal
+				var t = that.lvl[tY-1][tX];
 				if ((t==0)||(t==3)) {
 					that.lvl[tY][tX]-=4;
 					that.lvl[tY-1][tX]+=4;
 					tY-=1;
-				// elif box or boxgoal
 				} else if ((t==2)||(t==5)) {
-					// square past square above
 					var t1 = that.lvl[tY-2][tX];
-					// if empty or goal
 					if ((t1==0)||(t1==3)) {
 						that.lvl[tY][tX]-=4;
 						that.lvl[tY-1][tX]+=2;
@@ -52,19 +71,13 @@
 					}
 				}
 			} else if (d=='d') {
-				// square and square above
-				var c = that.lvl[tY][tX],
-					t = that.lvl[tY+1][tX];
-				// if targ empty or goal
+				var t = that.lvl[tY+1][tX];
 				if ((t==0)||(t==3)) {
 					that.lvl[tY][tX]-=4;
 					that.lvl[tY+1][tX]+=4;
 					tY+=1;
-				// elif box or boxgoal
 				} else if ((t==2)||(t==5)) {
-					// square past square above
 					var t1 = that.lvl[tY+2][tX];
-					// if empty or goal
 					if ((t1==0)||(t1==3)) {
 						that.lvl[tY][tX]-=4;
 						that.lvl[tY+1][tX]+=2;
@@ -73,19 +86,13 @@
 					}
 				}
 			} else if (d=='l') {
-				// square and square above
-				var c = that.lvl[tY][tX],
-					t = that.lvl[tY][tX-1];
-				// if targ empty or goal
+				var t = that.lvl[tY][tX-1];
 				if ((t==0)||(t==3)) {
 					that.lvl[tY][tX]-=4;
 					that.lvl[tY][tX-1]+=4;
 					tX-=1;
-				// elif box or boxgoal
 				} else if ((t==2)||(t==5)) {
-					// square past square above
 					var t1 = that.lvl[tY][tX-2];
-					// if empty or goal
 					if ((t1==0)||(t1==3)) {
 						that.lvl[tY][tX]-=4;
 						that.lvl[tY][tX-1]+=2;
@@ -95,8 +102,7 @@
 				}
 			} else if (d=='r') {
 				// square and square above
-				var c = that.lvl[tY][tX],
-					t = that.lvl[tY][tX+1];
+				var t = that.lvl[tY][tX+1];
 				// if targ empty or goal
 				if ((t==0)||(t==3)) {
 					that.lvl[tY][tX]-=4;
@@ -116,7 +122,7 @@
 				}
 			}
 		}
-		this.base = new rw.Rule(true,2);
+		this.base = new rw.Rule(true,0);
 		this.rule = function() {
 			if (keyDelay==0) {
 				(rw.key('ua')) ? (checkMove('u'), keyDelay=5) :
@@ -125,6 +131,22 @@
 				(rw.key('la')) ? (checkMove('l'), keyDelay=5) : true;
 			} else {
 				keyDelay--;
+			}
+			var victory = true;
+			for (var y=0, ylen=this.lvl.length; y<ylen; y++) {
+				for (var x=0, xlen=this.lvl.length; x<xlen; x++) {
+					if (this.lvl[y][x]==2) victory = false;
+				}
+			}
+			if (victory) {
+				if (curLvl<lvls.length-1){
+					alert("Level "+(curLvl+1)+" vanquished!");
+					makeGame(++curLvl);
+				} else {
+					rw.stop(function() {
+						alert('victory is yours!');
+					});
+				}
 			}
 		}
 	}
@@ -141,33 +163,38 @@
 			} else if (stat==3) {
 				img = 'area';
 			} else if (stat==4) {
-				img = 'u';
+				img = 'man';
 			} else if (stat==5) {
 				img = 'boxdone';
 			} else if (stat==7) {
-				img = 'u';
+				img = 'manarea';
 			}
 			this.base.changeSprite(img);
 		}
 	};
 
-	function start() {
-		rw.init(96,128).setFPS(30);
-		rw.newRule('game', new game(lvls[0]));
-		for (var y=0, ylen=lvls[0].length; y<ylen; y++) {
-			for (var x=0, xlen=lvls[0][y].length; x<xlen; x++) {
-				rw.newEnt(new square(y, x)).base.display(16*x,16*y);
+	function makeGame(lv) {
+		rw.stop(function() {
+			rw.wipeAll();
+			rw.newRule('game', new game(lvls[lv]));
+			for (var y=0, ylen=lvls[lv].length; y<ylen; y++) {
+				for (var x=0, xlen=lvls[lv][y].length; x<xlen; x++) {
+					rw.newEnt(new square(y, x)).base.display(16*x,16*y);
+				}
 			}
-		}
-		rw.start();
+			rw.start();
+		});
+	}
 
-		//call make level;
+	var curLvl = 0;
+	function start() {
+		rw.init(320,240).setFPS(30);
+		rw.start();
+		makeGame(curLvl);
 	};
 	rw.loadSprites({
-		'u': ['sprites/d.png',16,16,0,0],
-		'd': ['sprites/d.png',16,16,0,0],
-		'l': ['sprites/d.png',16,16,0,0],
-		'r': ['sprites/d.png',16,16,0,0],
+		'man': ['sprites/man.png',16,16,0,0],
+		'manarea': ['sprites/manarea.png',16,16,0,0],
 		'none': ['sprites/none.png',16,16,0,0],
 		'area': ['sprites/area.png',16,16,0,0],
 		'box': ['sprites/box.png',16,16,0,0],
